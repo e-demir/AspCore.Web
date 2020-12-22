b59be49a88e2caff4878cb433bf37fcfbd98a447
Emrullah <e177demir@gmail.com>
2020-12-23 00:29:59 +0300

    Course controller index and list actions added

---
+++ Controllers/CourseController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspCore.Web.Controllers
+{
+    public class CourseController : Controller
+    {
+        //course/index
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //course/list
+        public IActionResult List()
+        {
+            return View();
+        }
+    }
+}
