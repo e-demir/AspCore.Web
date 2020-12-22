df79e70c23419c89392a0e2da988261e98ca0199
Emrullah <e177demir@gmail.com>
2020-12-23 01:14:33 +0300

    styling added attendees listed validation done

--- Controllers/CourseController.cs
+++ Controllers/CourseController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AspCore.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
 
 namespace AspCore.Web.Controllers
 {
@@ -7,13 +10,37 @@ namespace AspCore.Web.Controllers
         //course/index
         public IActionResult Index()
         {
+            var message = DateTime.Now.Hour > 12 ? "Tünaydın" : "Günaydın";
+            ViewBag.Message = message;
+
             return View();
         }
 
         //course/list
         public IActionResult List()
         {
+            var students =  Repository.Students.Where(c => c.WillAttend == true);
+            return View(students);
+        }
+
+        [HttpGet]
+        public IActionResult Apply()
+        {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Apply(Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                Repository.AddStudent(student);
+                return View("Thanks", student);
+            }
+            else
+            {
+                return View();
+            }
         }
     }
 }
---
+++ Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspCore.Web.Models
+{
+    public class Student
+    {
+        [Required(ErrorMessage ="İsim zorunludur")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Email zorunludur")]
+        [EmailAddress(ErrorMessage ="Geçerli bir mail adresi giriniz")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Telefon zorunludur")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Katılım durumu belirtiniz")]
+        public bool? WillAttend { get; set; }
+    }
+}
